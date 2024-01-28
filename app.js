3ee1f9bcfd4c42131033d9f562eaf92ebb11822c
Paulo Ricardo Alves <pauloricardooalves@gmail.com>
2024-01-28 14:23:46 -0300

    adiciona a funcionalidade de criptografar e descriptografar

---
+++ app.js
@@ -0,0 +1,57 @@
+function validarTexto(texto) {
+
+    texto = texto.replace(/\s/g, '')
+
+    if (/^[a-z]+$/g.test(texto)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function criptografar() {
+    let textArea = document.getElementById('textoOriginal');
+    let texto = textArea.value
+    
+    if (!validarTexto(texto)) {
+        return alert('O texto inserido deve conter apenas letras minúsculas sem acentos e sem caracteres especiais!');
+    }
+    
+    texto = texto.replace(/i/g, 'imes');
+    texto = texto.replace(/a/g, 'ai');
+    texto = texto.replace(/e/g, 'enter');
+    texto = texto.replace(/o/g, 'ober');
+    texto = texto.replace(/u/g, 'ufat');
+    
+    mostrarResposta(texto)
+    
+}
+
+function mostrarResposta(resposta) {
+    areaDeResposta = document.getElementById('areaParaResposta');
+
+    let respostaFormatada = '<textarea class="corpo__ladoDireito__resposta" rows="20" readonly>'
+        + resposta + 
+ '   </textarea>'
+
+    areaDeResposta.innerHTML = respostaFormatada
+
+}
+
+function descriptogrofar() {
+    let textArea = document.getElementById('textoOriginal');
+    let texto = textArea.value
+
+    if (!validarTexto(texto)) {
+        return alert('O texto inserido deve conter apenas letras minúsculas e sem acento!');
+    }
+
+    texto = texto.replace(/ai/g, 'a');
+    texto = texto.replace(/enter/g, 'e');
+    texto = texto.replace(/imes/g, 'i');
+    texto = texto.replace(/ober/g, 'o');
+    texto = texto.replace(/ufat/g, 'u');
+
+    mostrarResposta(texto)
+
+}
